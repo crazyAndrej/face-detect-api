3cd27c947449cbdad19019ebd68683ac0808a5ec
Andrej Lukasevic <forandrej@gmail.com>
2023-06-04 15:08:04 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-acute-10672',
     port: 5432,
     user: 'andrejslukasevics',
     password: '',
