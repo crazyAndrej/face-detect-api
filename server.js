6ce96aa1b98b57bf42c892e5b89fbdfe4c870e34
Andrej Lukasevic <forandrej@gmail.com>
2023-06-07 15:57:17 +0100

    update security

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'containers-us-west-53.railway.app',
-    port: 5563,
-    user: 'postgres',
-    password: 'oXH52bUDKeGS1zdAmRmt',
-    database: 'railway',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
 
